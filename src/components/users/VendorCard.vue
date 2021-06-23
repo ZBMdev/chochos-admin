7d362e3beaf56e9a0f002071794c6df663c16903
emmanuelojo <emmanuelojo1234@gmail.com>
2021-06-23 22:49:25 +0100

    Updated

---
+++ src/components/users/VendorCard.vue
@@ -0,0 +1,263 @@
+<template>
+  <PageHeading :title="`${artisan.fullName}'s Profile`"  style="display:flex; justify-content: center; margin-bottom: 30px" />
+  <ProgressSpinner style="display:flex; justify-content: center" v-if="loading" />
+  <div v-else>
+    <TabView :value="artisan">
+      <TabPanel header="About">
+        <div class="userCard">
+              <div
+                class="p-d-flex p-jc-center p-ai-center p-pt-4 p-pl-4 p-pr-4 p-pb-0"
+              >
+                <Avatar
+                  v-if="artisan.photoUrl === '' || artisan.photoUrl === null " 
+                  icon="pi pi-user"
+                  class="p-mr-2"
+                  style="background-color:#c8e6c9;color:#256029;width:8rem;height:8rem;font-size:4rem;"
+                  shape="circle"
+                />
+                <img v-else-if="artisan.photoUrl != '' || artisan.photoUrl != null "
+                  :src="artisan.photoUrl"
+                  :alt="artisan.photoUrl"
+                  style="width:8rem;height:8rem;font-size:4rem;"
+                />
+                <Avatar 
+                  v-else
+                  :label="artisan.fullName.charAt(0).toUpperCase()"
+                  class="p-mr-2"
+                  style="background-color:#c8e6c9;color:#256029;width:8rem;height:8rem;font-size:4rem;"
+                  shape="circle"
+                />
+                
+              </div>
+              <div  class="p-text-left" id="personalDetails">
+                <p>
+                  Name: <b>{{ artisan?.fullName }}</b>
+                </p>
+                <p>
+                  Username: <b>{{ "@" + artisan?.username }}</b>
+                </p>
+                <p>
+                  Email: <b>{{ artisan?.email }}</b>
+                </p>
+                <p>
+                  Phone number: <b>{{ artisan?.mobile }}</b>
+                </p>
+                <p>
+                  Address: <b>{{ artisan?.address }}</b>
+                </p>
+                <p>
+                  Date registered: <b>{{ artisan?.created_on }}</b>
+                </p>
+                <p>
+                  Last login: <b>{{ artisan?.lastLogin }}</b>
+                </p>
+                <p>
+                  Active: <b>{{ artisan?.isActive }}</b>
+                </p>
+                <p>
+                  About: <b>{{ artisan?.about }}</b>
+                </p>
+                <p>
+                  Languages: <b>{{ artisan?.languages }}</b>
+                </p>
+                <p>
+                  Address: <b>{{ artisan?.address }}</b>
+                </p>
+                <p>
+                  State: <b>{{ artisan?.state }}</b>
+                </p>
+                <p>
+                  City: <b>{{ artisan?.city }}</b>
+                </p>
+              </div>
+        </div>
+      </TabPanel>
+      <TabPanel header="Portfolio">
+        <div v-for="(portfolio, index) in portfolios" :key="index">
+          <Card style="width: 25em">
+            <template #header>
+                <img :src="portfolio.url" style="height: 15rem" />
+            </template>
+            <template #subtitle class="subtitle">
+                {{ portfolio.name }}
+            </template>
+            <template #content>
+              {{ portfolio.description }}
+            </template>
+          </Card>
+        </div>
+        <div>
+          <Card style="width: 25em">
+            <template #header>
+                <img src="" style="height: 15rem" />
+            </template>
+            <template #subtitle class="subtitle">
+                Card subtitle
+            </template>
+            <template #content>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+                quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
+            </template>
+          </Card>
+        </div>
+      </TabPanel>
+      <TabPanel header="Reviews">
+        <div>
+          <div class="review-card">
+            No reviews
+          </div>
+        </div>
+      </TabPanel>
+    </TabView>
+  </div>
+
+  <!--<div class="p-d-flex p-jc-center p-ai-center">
+    <div class="p-col-12 p-md-6">
+      <Card
+        class="p-text-center"
+        :value="artisan"
+      >
+        <template #header>
+          <div
+            class="p-d-flex p-jc-center p-ai-center p-pt-4 p-pl-4 p-pr-4 p-pb-0"
+          >
+            <Avatar
+              v-if="artisan.photoUrl === '' || artisan.photoUrl === null " 
+              icon="pi pi-user"
+              class="p-mr-2"
+              style="background-color:#c8e6c9;color:#256029;width:8rem;height:8rem;font-size:4rem;"
+              shape="circle"
+            />
+            <Avatar 
+              v-else-if="artisan.fullName"
+              :label="artisan.fullName.charAt(0).toUpperCase()"
+              class="p-mr-2"
+              style="background-color:#c8e6c9;color:#256029;width:8rem;height:8rem;font-size:4rem;"
+              shape="circle"
+            />
+            <img v-else
+              :src="artisan.photoUrl"
+              :alt="artisan.photoUrl"
+              style="width:8rem;height:8rem;font-size:4rem;"
+            />
+          </div>
+        </template>
+        <template #content>
+          <div class="p-text-left">
+            <p>
+              Name: <b>{{ artisan?.fullName }}</b>
+            </p>
+            <p>
+              Username: <b>{{ "@" + artisan?.username }}</b>
+            </p>
+            <p>
+              Email: <b>{{ artisan?.email }}</b>
+            </p>
+            <p>
+              Products: <b>{{ artisan?.about }}</b>
+            </p>
+            <p>
+              Address: <b>{{ artisan?.address }}</b>
+            </p>
+          </div>
+        </template>
+      </Card>
+    </div>
+  </div>-->
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Options, Vue } from 'vue-class-component';
+import MainLayout from '@/components/layouts/MainLayout.vue';
+import Artisan from '@/models/Artisan';
+import ArtisanService from '@/services/ArtisanService';
+import { ArtisanData } from '@/types/artisan'
+import BombsightService from '@/services/BombsightService';
+import { useToast } from 'primevue/usetoast';
+import qs from 'qs';
+import { reactive } from 'vue';
+import DynamicForm from "@/components/elements/DynamicForm.vue";
+import { profileFormSchema, passwordFormSchema } from '@/models/Admin';
+
+
+@Options<ArtisanCard>({
+  components: { DynamicForm  },
+})
+
+export default class ArtisanCard extends Vue {
+
+  isLoading = false;
+  artisans: Artisan[] = [];
+  artisan = reactive(new Artisan({})) as Artisan;
+  datasource: Artisan[] = [];
+  totalRecords = 0;
+  service: ArtisanService = new ArtisanService();
+  selectedArtisans: Artisan[] = [];
+  filters: Record<string, unknown> = {};
+  submitted = false;
+  toast = useToast();
+  imageService = new BombsightService();
+  imageLoading = false;
+  firstRecordIndex = 0;
+  rowstoDisplay = 10;  
+
+
+  created() {
+    // watch the params of the route to fetch the data again
+    this.$watch(
+      () => this.$route.params,
+      () => {
+        if (this.thereIsAnID) {
+          this.getData();
+        }
+      },
+      // fetch the data when the view is created and the data is
+      // already being observed
+      { immediate: true }
+    )
+  }
+
+  get thereIsAnID() {
+    return !!this.$route.params.id
+  }
+
+  getData() {
+    this.getArtisan();
+  }
+
+  getArtisan() {
+    this.isLoading = true;
+    this.service.getOne(+this.$route.params.id)
+      .then((artisanData) => {
+        this.setArtisan(new Artisan(artisanData));
+        this.isLoading = false;
+      });
+  }
+
+  setArtisan(value: Artisan) {
+    this.artisan = reactive(value) as Artisan;
+  }
+}
+</script>
+
+<style scoped>
+.userCard{
+  width: 100%;
+  height: 100vh;
+}
+.p-text-center{
+  widows: 100%;
+}
+#userDetails {
+  display: flex;
+}
+#personalDetails{
+  float: left;
+  margin-left: 20px;
+}
+#jobDetails{
+  float: right;
+  margin-right: 350px;
+}
+</style>
